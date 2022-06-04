616bf775812da7fb36f8488d2d5e8d46fc8d9a4d
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-04 12:05:02 +0600

    require email verification

--- src/pages/Login/RequireAuth.js
+++ src/pages/Login/RequireAuth.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 import Loading from '../shared/Loading';
 
 
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
 
     if (loading) {
@@ -15,6 +17,21 @@ const RequireAuth = ({ children }) => {
 
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />
+    }
+    if (user.providerData[0]?.providerId === 'password' && !user.emailVerified) {
+        return <div className='verification-container'>
+            <h5>Verify Your Email Address</h5>
+            <p>Before proceeding, please check your email for a verification link. If you did not receive the email,</p>
+            <p className='link-paragraph'>
+                <span
+                    className='verify-link'
+                    onClick={async () => {
+                        await sendEmailVerification();
+                        toast('Verification Mail Send');
+                    }}
+                >click here to request another.</span>
+            </p>
+        </div>
     }
     return children;
 };
