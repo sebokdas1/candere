425c980e56b73df9ddb8896f1fee798121d0f610
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-04 11:55:10 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Login/Register';
+import Navbar from './pages/shared/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
-
+        <Route path='/' element={<Home />}></Route>
 
         <Route path='login' element={<Login />}></Route>
         <Route path='register' element={<Register />}></Route>
---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div>
+            <div class="carousel w-full">
+                <div id="slide1" class="carousel-item relative w-full">
+                    <img src="https://cfcdn20.candere.com/media/custom/banners/File-1653555389.jpg" class="w-full" alt='' />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" class="btn btn-circle"> <span className='arrow'>❮</span> </a>
+                        <a href="#slide2" class="btn btn-circle"> <span className='arrow'>❯</span> </a>
+                    </div>
+                </div>
+                <div id="slide2" class="carousel-item relative w-full">
+                    <img src="https://cfcdn20.candere.com/media/custom/banners/File-1654253696.gif" class="w-full" alt='' />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" class="btn btn-circle"> <span className='arrow'>❮</span> </a>
+                        <a href="#slide3" class="btn btn-circle"> <span className='arrow'>❯</span> </a>
+                    </div>
+                </div>
+                <div id="slide3" class="carousel-item relative w-full">
+                    <img src="https://cfcdn20.candere.com/media/custom/banners/File-1654081975.jpg" class="w-full" alt='' />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" class="btn btn-circle"> <span className='arrow'>❮</span> </a>
+                        <a href="#slide4" class="btn btn-circle"> <span className='arrow'>❯</span> </a>
+                    </div>
+                </div>
+                <div id="slide4" class="carousel-item relative w-full">
+                    <img src="https://cfcdn20.candere.com/media/custom/banners/File-1654082182.jpg" class="w-full" alt='' />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" class="btn btn-circle"> <span className='arrow'>❮</span> </a>
+                        <a href="#slide1" class="btn btn-circle"> <span className='arrow'>❯</span> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
